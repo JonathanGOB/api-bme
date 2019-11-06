7ae22452367a350f09f228b54f0a9c990569a3d7
Faimire <faimireg@hotmail.com>
2019-11-06 14:10:51 +0100

    database added

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/api/v1/sensors',  methods = ['GET'])
+def sensor():
+    if request.method == 'GET':
+        sensor = request.args.get('sensorname')
+        return sensor
 
+@app.route('/')
+def hello():
+    return 'hello world'
 
 if __name__ == '__main__':
     app.run()
