98abe0ca7a053687f8bbbe6457a90cd33d0d8d26
Faimire <faimireg@hotmail.com>
2019-11-24 20:01:07 +0100

    needs threading

--- mqtt-client.py
+++ mqtt-client.py
@@ -1,9 +1,18 @@
 import paho.mqtt.client as mqtt
 import json
-import pyodbc
+import threading
 
 
-buffer = {}
+
+buffer = {
+    "device_id": "",
+    "humidity": "",
+    "pressure": "",
+    "temperature": "",
+    "timestamp": "",
+}
+
+currentmessage = ""
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -16,36 +25,31 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    #put in database
-    print(msg.topic + " " + msg.payload.decode('utf-8'))
-    topic = str(msg.topic.split("/"))
+    msg = currentmessage
+
+def computing(msg):
+    print(buffer)
+    topic = msg.topic.split("/")
+    buffer["device_id"] = topic[1]
     message = json.load(str(msg.payload.decode('utf-8')))
     key = message.keys()[0]
     buffer[key] = message[key]
-    buffer ["device"] = topic[1]
-    if len(buffer) == 4 and key == "humidity":
-        insert = ""
-        values = ""
-        for x in buffer:
-            insert += x + ","
-            values += buffer[x] + ","
-
-        insert = insert[:-1]
-        values = values[:-1]
-
-        statement = "INSERT INTO "  + insert + "VALUES " + values
-        print("statement: " + statement)
-
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("127.0.0.1", 1883, 60)
+client.connect("127.0.0.1", 1883, keepalive=60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
+def main():
+    threadmqtt = threading.Thread(target=client.loop_forever())
+    threadmqtt.start()
+
+
+if __name__ == '__main__':
+    main()
 
