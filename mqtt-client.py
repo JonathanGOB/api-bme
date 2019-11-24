75f286198446c68480d8f25fe1cdd5f9916ea388
Faimire <faimireg@hotmail.com>
2019-11-24 22:02:59 +0100

    in progress

--- mqtt-client.py
+++ mqtt-client.py
@@ -1,18 +1,17 @@
 import paho.mqtt.client as mqtt
 import json
 import threading
+import urllib.request
 
-
-
-buffer = {
-    "device_id": "",
-    "humidity": "",
-    "pressure": "",
-    "temperature": "",
-    "timestamp": "",
+schema = {
+    "device_id": None,
+    "humidity": None,
+    "pressure": None,
+    "temperature": None,
+    "timestamp": None,
 }
 
-currentmessage = ""
+messages = []
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -25,15 +24,43 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    msg = currentmessage
+    #appends the data to the messages
+    messages.append(msg)
 
-def computing(msg):
-    print(buffer)
+def computing():
+    print(schema)
+    #Format the data to put in the schema
+    msg = messages.pop()
     topic = msg.topic.split("/")
-    buffer["device_id"] = topic[1]
+    schema["device_id"] = topic[1]
     message = json.load(str(msg.payload.decode('utf-8')))
     key = message.keys()[0]
-    buffer[key] = message[key]
+    schema[key] = message[key]
+
+    #Sends data if complete and resets the schema if not. Removes the start incompletion data
+    if message[key] == "humidity" and not all(schema.values()):
+        print("POST to api")
+        request = urllib.request.Request("http://127.0.0.1:5000/Api/V1/CapturedData")
+        request.add_header('Content-Type', 'x-www-form-urlencoded; charset=utf-8')
+        data = json.dumps(schema)
+        dataBytes = data.encode('utf-8')
+        request.add_header('Content-length', len(dataBytes))
+        response = urllib.request.urlopen(request, dataBytes)
+        print(response)
+        resetSchema()
+
+    elif messages[key] == "humidity" and all(schema.values()):
+        resetSchema()
+
+#resets the schema
+def resetSchema():
+    schema = {
+        "device_id": None,
+        "humidity": None,
+        "pressure": None,
+        "temperature": None,
+        "timestamp": None,
+    }
 
 client = mqtt.Client()
 client.on_connect = on_connect
