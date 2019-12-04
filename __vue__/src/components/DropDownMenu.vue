e5c26edb31ce2ab42a9073556ab9d959e42436ed
Faimire <faimireg@hotmail.com>
2019-12-04 22:26:25 +0100

    package lock remove

--- __vue__/src/components/DropDownMenu.vue
+++ __vue__/src/components/DropDownMenu.vue
@@ -22,7 +22,7 @@
             sendSelected: function(selected){
                 this.$emit('selected', selected)
             },
-            
+
             getOptions: function(){
                 axios.get(`http://127.0.0.1:5000/Api/V1/Devices`)
                     .then(response => {
