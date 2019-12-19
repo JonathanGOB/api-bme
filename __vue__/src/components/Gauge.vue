11b975ad4b9d271e51bd1bf62ecd5689c3e673d2
Faimire <faimireg@hotmail.com>
2019-12-19 17:07:33 +0100

    small addon

--- __vue__/src/components/Gauge.vue
+++ __vue__/src/components/Gauge.vue
@@ -1,16 +1,28 @@
 <template>
-    <b-table ref="table" hover :items="items" :fields="fields"></b-table>
+    <div class="overflow-auto">
+        <b-pagination
+                v-model="currentPage"
+                :total-rows="rows"
+                :per-page="perPage"
+                aria-controls="my-table"
+        ></b-pagination>
+        <b-table ref="table" hover :per-page="perPage"
+                 :current-page="currentPage"
+                 small :items="items" :fields="fields"></b-table>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Gauge",
         components:{
 
         },
 
         data(){
             return{
+                perPage: 3,
+                currentPage: 1,
+                name: "Gauge",
                 items: [],
                 fields: [
                     { key: 'id_num', label: 'id_num', sortable: true},
@@ -21,6 +33,12 @@
                     { key: 'timestamp', label: 'timestamp', sortable: true},
                     { key: 'uri', label: 'uri', sortable: false},
                 ],
+            }
+        },
+
+        computed: {
+            rows() {
+                return this.items.length
             }
         },
 
