1f9d0495e7020f090518838991ce9bdc027f9fee
KingLackland <faimireg@hotmail.com>
2019-12-19 15:21:27 +0100

    make your own config.py
    format:
    
    server = 
    database = 
    username = 
    password = 
    CONN_STRING = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+ server +';DATABASE='+database+';UID='+username+';PWD='+ password

--- config.py
+++
@@ -1,5 +0,0 @@
-server = 'tcp:iotservergr9.database.windows.net,1433'
-database = 'IoTdatabase'
-username = 'gerbenbunt'
-password = 'Wachtwoord123'
-CONN_STRING = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+ server +';DATABASE='+database+';UID='+username+';PWD='+ password
